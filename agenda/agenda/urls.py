d0353fd13a80bc7d89ada4a8ee8120fb275876d9
Alfredo de Morais <alfredneto@hotmail.com>
2021-06-07 22:27:56 -0300

    ultimo modulo

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     # lista de urls criada para aplicação
     # path('eventos/<titulo_evento>', views.eventos),
     path('agenda/', views.lista_eventos),
+    path('agenda/lista/', views.json_lista_evento),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
     path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
