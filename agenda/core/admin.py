7619902939f12589a5b35cbde54a64c4e6281ef5
Alfredo de Morais <alfredneto@hotmail.com>
2021-06-07 21:49:57 -0300

    inserido funcionalidade de alteração e exclusão de compromissos

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -5,7 +5,7 @@ from core.models import Evento
 
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_criacao', 'data_evento')
+    list_display = ('id', 'titulo', 'data_criacao', 'data_evento')
     list_filter = ('usuario', 'data_evento')
 
 
